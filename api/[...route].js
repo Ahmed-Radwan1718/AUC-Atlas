130a4c4ab3ee12e6a9225692b46bc8da023d29f8
Ahmed-Radwan1718 <ahmedradwan21@gmail.com>
2026-07-31 23:52:34 +0300

    Create [...route].js

---
+++ api/[...route].js
@@ -0,0 +1,32 @@
+const routeLoaders = {
+  "login": function () { return require("../server/routes/login"); },
+  "logout": function () { return require("../server/routes/logout"); },
+  "me": function () { return require("../server/routes/me"); }
+};
+
+function getRouteName(req) {
+  if (req.query && req.query.route) {
+    return Array.isArray(req.query.route)
+      ? req.query.route.join("/")
+      : String(req.query.route || "");
+  }
+
+  const rawUrl = String(req.url || "");
+  const pathOnly = rawUrl.split("?")[0];
+
+  return decodeURIComponent(pathOnly)
+    .replace(/^\/api\/?/, "")
+    .replace(/^\/+/, "")
+    .replace(/\/+$/, "");
+}
+
+module.exports = async function handler(req, res) {
+  const routeName = getRouteName(req);
+  const routeLoader = routeLoaders[routeName];
+
+  if (!routeLoader) {
+    return res.status(404).json({ error: "API route not found." });
+  }
+
+  return routeLoader()(req, res);
+};
