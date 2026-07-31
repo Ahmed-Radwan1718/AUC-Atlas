62e1bf88fe10096cae344e7ca799372366beae13
Ahmed-Radwan1718 <ahmedradwan21@gmail.com>
2026-07-31 23:53:28 +0300

    Create logout.js

---
+++ server/routes/logout.js
@@ -0,0 +1,15 @@
+const {
+  clearSiteSessionCookie
+} = require("../_lib/securityHelpers");
+
+module.exports = async function handler(req, res) {
+  if (req.method !== "POST" && req.method !== "GET") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  clearSiteSessionCookie(res);
+
+  return res.status(200).json({
+    success: true
+  });
+};
