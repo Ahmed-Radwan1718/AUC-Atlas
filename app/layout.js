a08bfbc494944c3fa6f6df20e8946cecfcb19ce5
Ahmed-Radwan1718 <ahmedradwan21@gmail.com>
2026-08-07 01:35:12 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "AUC Atlas | Professors, Courses & Student Materials"
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
